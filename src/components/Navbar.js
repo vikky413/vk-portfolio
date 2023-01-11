ab438f380ce0213ddbc2836d0c3263cd4f46c31f
Vikram Singh <vikramsinghrewa2001@gmail.com>
2023-01-11 19:53:21 +0530

    correct

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -105,7 +105,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/vikky413/vk-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
