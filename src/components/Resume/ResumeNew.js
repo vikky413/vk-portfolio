776faa7246473ab567ce22c71aa1943cdd1b6c98
VIKRAM SINGH <67967129+vikky413@users.noreply.github.com>
2025-01-06 00:15:07 +0530

    Update with latest resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/vikky413/vk-portfolio/main/src/Assets/ResumeVk.pdf";
+  "https://raw.githubusercontent.com/vikky413/vk-portfolio/main/src/Assets/Vikram_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
